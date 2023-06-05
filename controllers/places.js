a3e00fbc11e7766b51d0c8385e0642ddc562bd36
HernandezJC <jchernandez1021@gmail.com>
2023-06-05 01:39:47 -0700

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
     let places = [{
@@ -20,8 +21,19 @@ router.get('/', (req, res) => {
       })
       
       router.post('/', (req, res) => {
-        console.log(req.body)
-        res.send('POST /places')
+        // console.log(req.body)
+        if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+          req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+          req.body.state = 'USA'
+        }
+        places.push(req.body)
+        res.redirect('/places')
       })
       
     res.render('places/index', {places})
@@ -31,3 +43,6 @@ module.exports = router
 
 
 // Ask how to make pictures smaller
+
+
+
