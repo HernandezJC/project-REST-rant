61079c818ddb9cadef61d17f66ce549d5eac895d
HernandezJC <jchernandez1021@gmail.com>
2023-06-05 20:28:01 -0700

    part 5 error

--- index.js
+++ index.js
@@ -22,15 +22,18 @@ require('dotenv').config()
 
 
 //Henry Index.js
+//Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use(methodOverride('_method'))
 
 // app.get('/', (req, res) => {
 //     res.send('Hello world!')
