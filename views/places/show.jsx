99e432ace8d7403bab339981a7c9962831659a4b
HernandezJC <jchernandez1021@gmail.com>
2023-06-13 19:08:30 -0700

    created and used helper method, changes in models/places.js, and show.jsx

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,9 +5,22 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{ data.place.name }</h1>
+            <div className="row">
+              <div className="col-sm-6">
+                <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+            </div>
+            <div className="col-sm-6">
             <h2>Rating</h2>
             <h2>Description</h2>
+            <h3>
+              {data.place.showEstablished() }
+            </h3>
+            <h3>
+              Serving {data.place.cuisines}
+            </h3>
             <h2>Comments</h2>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
               Edit
@@ -16,7 +29,9 @@ function show (data) {
               <button type="submit" className="btn btn-danger">
                 Delete
               </button>
-            </form>     
+            </form>
+            </div>
+            </div>
           </main>
         </Def>
     )
