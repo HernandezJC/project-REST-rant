5dd2a0e51beb4237fff4298489ac29e31688ae99
HernandezJC <jchernandez1021@gmail.com>
2023-06-16 04:25:38 -0700

    created rant model, put in comment seed data, and populated the comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,24 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
+          <h4>{c.content}</h4>
+          <h3><stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
     return (
         <Def>
           <main>
@@ -22,6 +40,7 @@ function show (data) {
               Serving {data.place.cuisines}
             </h3>
             <h2>Comments</h2>
+            {comments}
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
               Edit
             </a>  
