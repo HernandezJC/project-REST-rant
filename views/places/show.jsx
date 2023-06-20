de38a28ae9b0a3617142263abd1c28f2df28d80a
HernandezJC <jchernandez1021@gmail.com>
2023-06-20 09:18:39 -0700

    Implemented PUT and DELETE, error with edit button

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,24 @@ function show (data) {
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated</h3>
+  )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i <averageRating; i++) {
+      stars += '⭐️'
+    }
+    rating = (
+      <h3>
+        {stars} stars
+      </h3>
+    )
     comments = data.place.comments.map(c => {
       return (
         <div className="border">
@@ -52,6 +69,34 @@ function show (data) {
             </form>
             </div>
             </div>
+            <h2>Does {data.place.name} deserve a Rave 💖 or a Rant 🤬?</h2>
+            <form action={`/places/${data.place.id}/comment`} method="POST">
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label htmlFor="content">Content</label>
+                  <textarea id="content" name="content" className="form-control"></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="author">Author</label>
+                  <input id="author" name="author" className="form-control" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="stars">⭐️ Star Rating</label>
+                  <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+                </div>
+                <div className="form-group col-sm-2">
+                  <label htmlFor="rant">Rant?</label>
+                  <input type="checkbox" id="rant" name="rant" className="form-control" />
+                </div>
+                {/* <div className="form-group col-sm-2">
+                  <label htmlFor="recommend">Recommend?</label>
+                  <input type="checkbox" id="recommend" name="recommend" className="form-control" />
+                </div> */}
+              </div>
+              <input type="submit" className="btn btn-primary" value="Add Comment" />
+              </form>
           </main>
         </Def>
     )
