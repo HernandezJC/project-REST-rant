f648cab54c4c797c29dc49013e20e1e45637e48e
HernandezJC <jchernandez1021@gmail.com>
2023-06-26 16:32:36 -0700

    seeded data

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-// //Modules and Globals
-// require('dotenv').config()
-// const express = require('express')
-// const app = express()
-// const methodOverride = require('method-override')
-
-// app.set('view engine', 'jsx')
-// app.engine('jsx', require('express-react-views').createEngine())
-// app.use(methodOverride('_method'))
-// app.use('/places', require('./controllers/places'))
-// app.use(express.urlencoded({ extended: true }))
-// app.use(express.static('public'))
-
-
-// // app.get('/', (req, res) => {
-// //     res.send('Hello world!')
-// // })
-
-// app.get('/', (req, res) => {
-//     res.render('home')
-//   })
-  
-// app.get('*', (req, res) => {
-//     res.render('error404')
-// })
-
-// app.listen(process.env.PORT)
-
-
-
-
-//
 // Modules and Globals
 require('dotenv').config()
 const express = require('express')
